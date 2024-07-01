f6889a6090fe47573c0c55e2fee09d9650e26f08
ISABELY AMABILE GOULART <isabely_goulart@estudante.sesisenai.org.br>
2024-07-01 11:51:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import BuscarCep from './components/BuscarCep'
+
+function App() {
+
+
+  return (
+    <>
+      <BuscarCep> </BuscarCep>
+    </>
+  )
+}
+
+export default App
